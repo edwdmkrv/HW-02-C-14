040c460d1495319dc13feb57ff1137cfe1c18017
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-10 02:41:06 +0300

    test1.cpp: port test1 to the GoogleTest framework

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,14 @@
 #include "lib.hpp"
 
-#include <cstdlib>
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
-	return ver() == std::to_string(major()) + '.' + std::to_string(minor()) + '.' + std::to_string(patch()) ? EXIT_SUCCESS : EXIT_FAILURE;
+#include <string>
+
+TEST(Group1, Test1) {
+	EXPECT_EQ(ver(), std::to_string(major()) + '.' + std::to_string(minor()) + '.' + std::to_string(patch()));
+}
+
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
