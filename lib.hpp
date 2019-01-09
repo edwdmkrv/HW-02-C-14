d47bb7c37d375cc7a9727284d340a945478b597c
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-10 02:19:52 +0300

    ipfilter.cpp, lib.cpp, lib.hpp: move the base functionality to the library

--- lib.hpp
+++ lib.hpp
@@ -2,11 +2,23 @@
 #define __LIB_INCLUDED
 
 #include <string>
+#include <vector>
+#include <functional>
+#include <iostream>
 
 std::string ver();
 
 unsigned major();
 unsigned minor();
 unsigned patch();
+
+using ip_t = std::vector<std::string>;
+using ip_pool_t = std::vector<ip_t>;
+using filter_t = std::function<bool(ip_t const &)>;
+
+ip_t split(std::string const &str, char const d);
+ip_pool_t parse(std::istream &i);
+void sort(ip_pool_t &ip_pool);
+void issue(std::ostream &o, ip_pool_t const &ip_pool, filter_t const &filter);
 
 #endif
