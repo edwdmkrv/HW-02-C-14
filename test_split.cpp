6492c81037e22d070e4558c23f571305662eebf9
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-13 23:37:48 +0300

    test_split.cpp: implement the test for split

--- test_split.cpp
+++ test_split.cpp
@@ -2,10 +2,39 @@
 
 #include <gtest/gtest.h>
 
+#include <cstdint>
+#include <limits>
+#include <sstream>
+#include <random>
 #include <string>
 
 TEST(TestLibrary, TestFunctionSplit) {
-	EXPECT_TRUE(true);
+	enum: std::size_t {len = 1024 * 1024};
+
+	std::uniform_int_distribution<char> dist{std::numeric_limits<char>::min(), std::numeric_limits<char>::max()};
+	std::random_device rnd;
+
+	std::string str;
+	std::vector<std::string> ethalon{{}};
+
+	char const d{dist(rnd)};
+
+	for (std::size_t n{0}; n < len; n++) {
+		char const sym{dist(rnd)};
+
+		str += sym;
+
+		if (sym != d) {
+			ethalon.back() += sym;
+		} else {
+			ethalon.push_back({});
+		}
+	}
+
+	std::vector<std::string> const splitted{split(str, d)};
+	std::cout << "size: " << splitted.size() << std::endl;
+
+	EXPECT_EQ(splitted, ethalon);
 }
 
 int main(int argc, char *argv[]) {
