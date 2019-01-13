52be034d0b84443151c7c6abf643d55917fb443f
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-14 02:49:33 +0300

    test_sort.cpp: add the test for sort

--- test_sort.cpp
+++ test_sort.cpp
@@ -2,8 +2,56 @@
 
 #include <gtest/gtest.h>
 
+#include <cstdint>
+#include <limits>
+#include <sstream>
+#include <random>
+#include <string>
+
 TEST(GroupiLibrary, TestFunctionSort) {
-	EXPECT_TRUE(true);
+	enum: unsigned {attempts = 10};
+
+	for (unsigned attempt{0}; attempt < attempts; attempt++) {
+		enum: unsigned {lines = 4 * 1024};
+
+		std::uniform_int_distribution<uint8_t> dist{std::numeric_limits<uint8_t>::min(), std::numeric_limits<uint8_t>::max()};
+		std::random_device rnd;
+
+		std::vector<uint32_t> ips;
+
+		for (unsigned line{0}; line < lines; line++) {
+			ips.emplace_back(dist(rnd) + dist(rnd) * 256u + dist(rnd) * 256u * 256 + dist(rnd) * 256u * 256 * 256);
+		}
+
+		auto const convert{[&] {
+			ip_pool_t pool;
+
+			for (unsigned line{0}; line < lines; line++) {
+				uint32_t const ip{ips.at(line)};
+
+				pool.emplace_back(
+					ip_t{
+						std::to_string(ip / 256 / 256 / 256),
+						std::to_string(ip / 256 / 256 % 256),
+						std::to_string(ip / 256 % 256),
+						std::to_string(ip % 256)
+					}
+				);
+			}
+
+			return pool;
+		}};
+
+		ip_pool_t to_be_sorted{convert()};
+
+		std::sort(std::begin(ips), std::end(ips), std::greater<uint32_t>{});
+
+		ip_pool_t const sorted{convert()};
+
+		sort(to_be_sorted);
+
+		EXPECT_EQ(to_be_sorted, sorted);
+	}
 }
 
 int main(int argc, char *argv[]) {
