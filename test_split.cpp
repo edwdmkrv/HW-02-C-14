0068931a2fda3df30134423b077db71f06441568
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-13 23:42:11 +0300

    test_split.cpp: enhard the test

--- test_split.cpp
+++ test_split.cpp
@@ -9,32 +9,36 @@
 #include <string>
 
 TEST(TestLibrary, TestFunctionSplit) {
-	enum: std::size_t {len = 1024 * 1024};
+	enum: unsigned {attempts = 10};
 
-	std::uniform_int_distribution<char> dist{std::numeric_limits<char>::min(), std::numeric_limits<char>::max()};
-	std::random_device rnd;
-
-	std::string str;
-	std::vector<std::string> ethalon{{}};
-
-	char const d{dist(rnd)};
-
-	for (std::size_t n{0}; n < len; n++) {
-		char const sym{dist(rnd)};
-
-		str += sym;
-
-		if (sym != d) {
-			ethalon.back() += sym;
-		} else {
-			ethalon.push_back({});
+	for (unsigned attempt{0}; attempt < attempts; attempt++) {
+		enum: std::size_t {len = 1024 * 1024};
+	
+		std::uniform_int_distribution<char> dist{std::numeric_limits<char>::min(), std::numeric_limits<char>::max()};
+		std::random_device rnd;
+	
+		std::string str;
+		std::vector<std::string> ethalon{{}};
+	
+		char const d{dist(rnd)};
+	
+		for (std::size_t n{0}; n < len; n++) {
+			char const sym{dist(rnd)};
+	
+			str += sym;
+	
+			if (sym != d) {
+				ethalon.back() += sym;
+			} else {
+				ethalon.push_back({});
+			}
 		}
+	
+		std::vector<std::string> const splitted{split(str, d)};
+		std::cout << "size: " << splitted.size() << std::endl;
+	
+		EXPECT_EQ(splitted, ethalon);
 	}
-
-	std::vector<std::string> const splitted{split(str, d)};
-	std::cout << "size: " << splitted.size() << std::endl;
-
-	EXPECT_EQ(splitted, ethalon);
 }
 
 int main(int argc, char *argv[]) {
