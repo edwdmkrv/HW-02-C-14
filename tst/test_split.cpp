976c22646f269d8b73a8b101ca15f0dc99ebecad
edwdmkrv <46138366+edwdmkrv@users.noreply.github.com>
2019-01-29 01:48:59 +0300

    test_split.cpp: rewrite the test as a fixture with the handy dataset

--- tst/test_split.cpp
+++ tst/test_split.cpp
@@ -7,37 +7,51 @@
 #include <random>
 #include <string>
 
-TEST(TestLibrary, TestFunctionSplit) {
-	enum: unsigned {attempts = 10};
+struct Data {
+	char const delimiter;
+	std::string const string;
+	ip_str_t const expected;
 
-	for (unsigned attempt{0}; attempt < attempts; attempt++) {
-		enum: std::size_t {len = 1024 * 1024};
-	
-		std::uniform_int_distribution<char> dist{std::numeric_limits<char>::min(), std::numeric_limits<char>::max()};
-		std::random_device rnd;
-	
-		std::string str;
-		ip_str_t ethalon{{}};
-	
-		char const d{dist(rnd)};
-	
-		for (std::size_t n{0}; n < len; n++) {
-			char const sym{dist(rnd)};
-	
-			str += sym;
-	
-			if (sym != d) {
-				ethalon.back() += sym;
-			} else {
-				ethalon.push_back({});
-			}
-		}
-	
-		ip_str_t const splitted{split(str, d)};
-	
-		EXPECT_EQ(splitted, ethalon);
+};
+
+std::ostream &operator <<(std::ostream &o, ip_str_t const &ip_str) {
+	bool first{true};
+
+	o << '{';
+
+	for (auto const &str: ip_str) {
+		o << (first ? "" : ", ") << '"' << str << '"';
+		first = false;
 	}
+
+	return o << '}';
 }
+
+std::ostream &operator <<(std::ostream &o, Data const &data) {
+	return o
+		<< "delimiter: '" << data.delimiter << "', "
+		<< "string: \"" << data.string << "\", "
+		<< "expected: " << data.expected;
+}
+
+struct TestLibrary: testing::Test, testing::WithParamInterface<Data> {
+};
+
+TEST_P(TestLibrary, TestFunctionSplit) {
+	Data const data{GetParam()};
+
+	EXPECT_EQ(split(data.string, data.delimiter), data.expected);
+}
+
+INSTANTIATE_TEST_CASE_P(SimpleDataset, TestLibrary,
+	testing::Values(
+		Data{'.', "1.2.3.4", {"1", "2", "3", "4"}},
+		Data{'.', "1...4", {"1", "", "", "4"}},
+		Data{' ', "This is a text", {"This", "is", "a", "text"}},
+		Data{' ', "This is a very long text", {"This", "is", "a", "very", "long", "text"}},
+		Data{'\n', "The first line\n\nThe third line", {"The first line", "", "The third line"}}
+	)
+);
 
 int main(int argc, char *argv[]) {
 	testing::InitGoogleTest(&argc, argv);
